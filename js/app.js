2a22c4aaa0d6be7f5337a4653cc293a2797d0c3a
foysal ahmed <mdrad43@gmail.com>
2021-05-19 19:23:13 +0800

    added foreach method

--- js/app.js
+++ js/app.js
@@ -1,3 +1,48 @@
-let name = "foysal";
+const companises = [
+  {
+    name: "wrc",
+    catagory: "atumotive",
+    start: 2004,
+    end: "present",
+  },
+  {
+    name: "nagamotor",
+    catagory: "atomobile",
+    start: 2006,
+    end: "present",
+  },
+  {
+    name: "red-zon",
+    catagory: "atumotive",
+    start: 2003,
+    end: "present",
+  },
+  {
+    name: "nisan",
+    catagory: "car-wash",
+    start: 2004,
+    end: "present",
+  },
+  {
+    name: "ktm",
+    catagory: "body-painting",
+    start: 2005,
+    end: "present",
+  },
+  {
+    name: "other",
+    catagory: "atumotive",
+    start: 2004,
+    end: "present",
+  },
+];
 
-console.log(name);
+const ages = [30, 39, 44, 48, 37, 51];
+
+companises.forEach(function (company) {
+  console.log(company.name);
+});
+
+ages.forEach(function (x) {
+  console.log(x);
+});
